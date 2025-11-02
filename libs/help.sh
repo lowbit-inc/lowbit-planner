cfab865c5a855d1446023c5ce8f619aa57cd73f7
Eric Guimarães de Sousa Silva <lowbit@icloud.com>
2025-11-02 14:33:28 +0000

    Minor change in help message

--- libs/help.sh
+++ libs/help.sh
@@ -17,26 +17,27 @@ function helpMessage() {
   echo "  $(basename $0) engage"
   echo
   echo "Ground:"
-  echo "  $(basename $0) inbox SUB_COMMAND"
-  echo "  $(basename $0) task SUB_COMMAND"
-  echo "  $(basename $0) recurring SUB_COMMAND"
-  echo "  $(basename $0) collection SUB_COMMAND"
+  echo "  $(basename $0) inbox"
+  echo "  $(basename $0) task"
+  echo "  $(basename $0) recurring"
+  echo "  $(basename $0) habit"
+  echo "  $(basename $0) collection"
   echo
   echo "Horizon 1:"
-  echo "  $(basename $0) project SUB_COMMAND"
+  echo "  $(basename $0) project"
   echo
   echo "Horizon 2:"
-  echo "  $(basename $0) area SUB_COMMAND"
+  echo "  $(basename $0) area"
   echo
   echo "Horizon 3:"
-  echo "  $(basename $0) goal SUB_COMMAND"
+  echo "  $(basename $0) goal"
   echo
   echo "Horizon 4:"
-  echo "  $(basename $0) vision SUB_COMMAND"
+  echo "  $(basename $0) vision"
   echo
   echo "Horizon 5:"
-  echo "  $(basename $0) purpose SUB_COMMAND"
-  echo "  $(basename $0) principle SUB_COMMAND"
+  echo "  $(basename $0) purpose"
+  echo "  $(basename $0) principle"
   echo
 }
 
