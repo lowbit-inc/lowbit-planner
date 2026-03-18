4840c366f4d620ad224348f13f6b08af0748d10a
Eric Guimarães de Sousa Silva <eric.gssilva@gmail.com>
2026-03-17 23:20:39 -0300

    Adding unit test for inbox (root)

--- tests/unit-tests.sh
+++ tests/unit-tests.sh
@@ -11,8 +11,9 @@ log_print info "Starting test scenarios"
 
 # Test Scenarios
 test_command_output "Libs - Utils - Datetime - Get week from date" "datetime_get_week_from_date 2026-03-17" "2026W11"
-test_command_output "Global Command - help"     "./plan.sh help" "Lowbit Planner - Main Help"
-test_command_output "Global Command - version"  "./plan.sh version" "Lowbit Planner - Version: "
+test_command_output "System Command - help"     "./plan.sh help" "Lowbit Planner - Main Help"
+test_command_output "System Command - version"  "./plan.sh version" "Lowbit Planner - Version: "
+test_command_output "Inbox - main"  "./plan.sh inbox" "Lowbit Planner - Inbox"
 
 # End
-log_print info "End of test scenarios"
+log_print info "End of test scenarios - ${color_bold}${color_green}All Passed${color_reset}"
