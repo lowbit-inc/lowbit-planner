82243ec34c02c7bc4f17379ccf52570d7d97b724
Eric Guimarães de Sousa Silva <eric.gssilva@gmail.com>
2026-03-17 22:56:33 -0300

    Updating inbox help messages

--- libs/objects/inbox.sh
+++ libs/objects/inbox.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+##############
+# Properties #
+##############
+
+# N/A
+
+###########
+# Methods #
+###########
+
 function inbox_add() {
 
   log_message debug "Starting Inbox Add"
@@ -8,7 +18,7 @@ function inbox_add() {
     user_arg="${1}" && log_message debug "User arg: ${user_arg}"
   else
     log_message debug "No user arg provided - calling help message"
-    help_inbox_add
+    inbox_help_add
   fi
 
   this_inbox_item="$@"
@@ -21,6 +31,22 @@ function inbox_add() {
   fi
 }
 
+function inbox_add_help() {
+  log_message debug "Getting help message: inbox add"
+  printf "${color_bold}${system_long_name} - Inbox Add${color_reset}\n"
+  printf "\n"
+  printf "${color_bold}DESCRIPTION:${color_reset}\n"
+  printf "  Add items to the inbox.\n"
+  printf "\n"
+  printf "${color_bold}USAGE:${color_reset}\n"
+  printf "  ${color_underline}${system_basename}${color_reset} ${color_bold}${color_red}inbox ${color_green}add ${color_blue}ITEM_NAME${color_reset}\n"
+  printf "\n"
+  printf "${color_bold}ARGUMENTS:${color_reset}\n"
+  printf "  ${color_gray}(no supported args)${color_reset}\n"
+  printf "\n"
+  exit 0
+}
+
 function inboxDelete() {
   this_inbox_id="$1"
 
@@ -30,6 +56,26 @@ function inboxDelete() {
     echo "Error: missing inbox item ID."
     exit 1
   fi
+}
+
+function inbox_help() {
+  log_message debug "Getting help message: inbox"
+  printf "${color_bold}${system_long_name} - Inbox${color_reset}\n"
+  printf "\n"
+  printf "${color_bold}DESCRIPTION:${color_reset}\n"
+  printf "  The place to capture all of your ideas.\n"
+  printf "\n"
+  printf "${color_bold}USAGE:${color_reset}\n"
+  printf "  ${color_underline}${system_basename}${color_reset} ${color_bold}${color_red}inbox ${color_green}SUBCOMMAND${color_reset} ${color_gray}[${color_bold}${color_blue}ARGUMENTS${color_reset}${color_gray}]${color_reset}\n"
+  printf "\n"
+  printf "${color_bold}SUBCOMMANDS:${color_reset}\n"
+  printf "  add\n"
+  printf "  clarify\n"
+  printf "  delete\n"
+  printf "  edit\n"
+  printf "  list\n"
+  printf "\n"
+  exit 0
 }
 
 function inboxList() {
@@ -44,7 +90,7 @@ function inbox_main() {
     user_arg="${1}" && log_message debug "User arg: ${user_arg}"
   else
     log_message debug "No User arg provided - calling help message"
-    help_inbox
+    inbox_help
   fi
 
   case "${user_arg}" in
@@ -60,14 +106,14 @@ function inbox_main() {
       inboxDelete "$1"
       ;;
     "help")
-      help_inbox
+      inbox_help
       ;;
     "list")
       inboxList
       ;;
     *)
       log_message warn "Unknown command (${user_arg})"
-      help_inbox
+      inbox_help
       ;;
   esac
 }
